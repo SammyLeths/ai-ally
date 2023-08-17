c2ae99bdd92635c01f4f81ff8987a4af66944b68
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-08-17 12:53:09 +0100

    added react-hook-form and cloudinary image upload

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,15 +1,92 @@
 "use client";
 
+import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
-import { Categories } from "../../../../../../components/categories";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 
 interface CompanionFormProps {
   initialData: Companion | null;
   categories: Category[];
 }
 
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  description: z.string().min(1, {
+    message: "Description is required.",
+  }),
+  instructions: z.string().min(200, {
+    message: "Instructions requires at least 200 characters.",
+  }),
+  seed: z.string().min(200, {
+    message: "Seed requires at least 200 characters.",
+  }),
+  src: z.string().min(1, {
+    message: "Image is required.",
+  }),
+  categoryId: z.string().min(1, {
+    message: "Category is required.",
+  }),
+});
+
 const CompanionForm = ({ categories, initialData }: CompanionFormProps) => {
-  return <div>Companion Form</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      description: "",
+      instructions: "",
+      seed: "",
+      src: "",
+      categoryId: undefined,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General information about your Companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4">
+                <FormControl>Image Upload Component</FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CompanionForm;
---
+++ components/image-upload.tsx
@@ -0,0 +1,48 @@
+"use cleint";
+
+import { useEffect, useState } from "react";
+import { CldUploadButton } from "next-cloudinary";
+import Image from "next/image";
+
+interface ImageUploadProps {
+  value: string;
+  onChange: (src: string) => void;
+  disabled?: boolean;
+}
+
+export const ImageUplaod = ({
+  value,
+  onChange,
+  disabled,
+}: ImageUploadProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-4 w-full flex flex-col justify-center items-center">
+      <CldUploadButton
+        options={{
+          maxFiles: 1,
+        }}
+        uploadPreset="q1ymvypo"
+      >
+        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              src="/placeholder.svg"
+              alt="Upload"
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      </CldUploadButton>
+    </div>
+  );
+};
