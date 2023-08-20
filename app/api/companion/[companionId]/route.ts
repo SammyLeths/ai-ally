a0de9a17802d87b7709375f05f0877617bd64086
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-08-20 17:54:29 +0100

    added Message model

---
+++ app/api/companion/[companionId]/route.ts
@@ -0,0 +1,56 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!params.companionId) {
+      return new NextResponse("Companion ID is required", { status: 400 });
+    }
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    // TODO: Check for subscription
+
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.companionId,
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
