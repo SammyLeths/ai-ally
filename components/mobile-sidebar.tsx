8d396b751a7959abdd67722aed3e8ed66c8c9f91
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-08-29 10:09:36 +0100

    fixed mobile sidebar type error

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,14 +3,14 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "@/components/sidebar";
 
-const MobileSidebar = () => {
+const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = ({ isPro }: NavbarProps) => {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <MobileSidebar />
+        <MobileSidebar isPro={isPro} />
         <Link href="/">
           <h1
             className={cn(
