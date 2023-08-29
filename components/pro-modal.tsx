2a1c24ee88cc5b9b591fbeef07b19eec95abc448
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-08-29 09:29:17 +0100

    final project tested

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -19,6 +19,11 @@ export const ProModal = () => {
   const { toast } = useToast();
 
   const [loading, setLoading] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   const onSubscribe = async () => {
     try {
@@ -35,6 +40,10 @@ export const ProModal = () => {
       setLoading(false);
     }
   };
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
